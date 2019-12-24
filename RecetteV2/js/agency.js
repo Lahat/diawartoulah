37c3acca6f53ebe839fc6a80165d311f81431d10
Abdou Lahat <bamba34@hotmail.com>
2019-12-24 22:35:10 +0100

    site diawartoulah

---
+++ RecetteV2/js/agency.js
@@ -0,0 +1,106 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $('.navbar').addClass('d-none');
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $('.navbar').removeClass('d-none');
+  })
+  jQuery(document).ready(function($) {
+
+        $('#myCarousel').carousel({
+                interval: 5000
+        });
+
+        jQuery(document).ready(function($) {
+
+                $('#myCarousel').carousel({
+                        interval: 5000
+                });
+
+                //Handles the carousel thumbnails
+                $('[id^=carousel-selector-]').click(function () {
+                var id_selector = $(this).attr("id");
+                try {
+                    var id = /-(\d+)$/.exec(id_selector)[1];
+                    console.log(id_selector, id);
+                    jQuery('#myCarousel').carousel(parseInt(id));
+                } catch (e) {
+                    console.log('Regex failed!', e);
+                }
+            });
+                // When the carousel slides, auto update the text
+                $('#myCarousel').on('slid.bs.carousel', function (e) {
+                         var id = $('.item.active').data('slide-number');
+                        $('#carousel-text').html($('#slide-content-'+id).html());
+})(jQuery); // End of use strict
+$(function(){
+
+   $(".current").click(function(){
+
+        var thisFilter = $(this).attr("id") + ".html";
+
+			$("#gallery").load(thisFilter);
+
+			$("#categories a").removeClass("current");
+
+			$(this).addClass("current");
+
+        return false;
+
+   });
+
+   $(".filter").click(function(){
+
+        var thisFilter = $(this).attr("id") + ".html";
+
+			$("#gallery").load(thisFilter);
+
+			$("#categories a").removeClass("current");
+
+			$(this).addClass("current");
+
+        return false;
+
+   });
+});
